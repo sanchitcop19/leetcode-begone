d3ae47a7724e03ba95bc8adacdde26753a126222
Sanchit <sanchitcop19@Gmail.com>
2019-11-27 15:47:45 -0500

    Fixed most bugs and core functionality done

--- leetcode-ignore.js
+++ leetcode-ignore.js
@@ -1,7 +1,3 @@
-//background.console.log(input);
-//background.console.log(input.value);
-//background.console.log(input.value);
-
 document.addEventListener("DOMContentLoaded", function(){
     const button = document.getElementById('add-ignore');
     console.log(button);
@@ -10,11 +6,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let registerButton = function(button){
     console.log("Registering button");
-    const id = document.getElementById('ignore-id').value;
-    console.log(id);
-    button.addEventListener("click", function(id){
-        console.log("Setting storage");
-        chrome.storage.sync.set({'ignore-problems': [id]}, function(){console.log('Added ' + id + 'to local storage')})
+    button.addEventListener("click", function(){
+        const input = document.getElementById('ignore-id');
+        const id = input.value;
+        input.value = "";
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {action: "add-problem", pid: id}, function(response){
                 console.log(response);
--- leetcode.js
+++ leetcode.js
@@ -1,17 +1,22 @@
+// TODO:  Check for empty button press
+
 let table = null;
 
-const ids = new Set(['1119', '1108']);
-
+let ids = [];
+if (localStorage['ignore-problems']){
+    ids = JSON.parse("[" + localStorage['ignore-problems'] + "]");
+    console.log(localStorage["ignore-problems"]);
+}
+console.log(localStorage["ignore-problems"]);
 let poll_count = 0;
 
 let removeSpecificProblem = function(id){
-    let td_id = $("td:contains(" + id + ")")[0];
+    let td_id = $("td").filter(function(){return $(this).text() == id})[0];
     td_id.parentNode.hidden = true;
 }
 
 let removeProblems = function(){
     console.log("removeProblem");
-    console.log(chrome.storage.sync.get(['ignore-problems'], function(result){console.log(result.key)}));
     for (let id of ids){
         removeSpecificProblem(id);
     }
@@ -20,7 +25,7 @@ let removeProblems = function(){
 let pollTable = setInterval(function(){
     table = document.getElementsByTagName('table')[0];
     console.log(table);
-    if (table || poll_count > 5){
+    if (table || poll_count > 10){
         clearInterval(pollTable);
         removeProblems();
     }
@@ -30,5 +35,12 @@ let pollTable = setInterval(function(){
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
     console.log(msg);
-    console.log(sender)
+    if (msg.action == "add-problem"){
+        console.log("inside message action")
+        console.log(localStorage["ignore-problems"]);
+        //chrome.storage.sync.set({'ignore-problems': msg.pid}, function(){console.log('Added ' + msg.pid + 'to local storage')})
+        localStorage['ignore-problems'] = (localStorage['ignore-problems'] === undefined? '' : localStorage['ignore-problems'] + ',' ) + msg.pid;
+        console.log(localStorage["ignore-problems"]);
+        removeSpecificProblem(msg.pid);
+    }
 })
